1e01d8bf529bf3fe8a0a3898f90f829ada47ea1a
unknown <guikhato@gmail.com>
2019-02-13 22:32:07 +0600

    First commit

---
+++ Weather/Forecast.cs
@@ -0,0 +1,218 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public class Location
+    {
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("region")]
+        public string region { get; set; }
+
+        [JsonProperty("country")]
+        public string country { get; set; }
+
+        [JsonProperty("lat")]
+        public double lat { get; set; }
+
+        [JsonProperty("lon")]
+        public double lon { get; set; }
+
+        [JsonProperty("tz_id")]
+        public string tz_id { get; set; }
+
+        [JsonProperty("localtime_epoch")]
+        public int localtime_epoch { get; set; }
+
+        [JsonProperty("localtime")]
+        public string localtime { get; set; }
+    }
+
+    public class Condition
+    {
+
+        [JsonProperty("text")]
+        public string text { get; set; }
+
+        [JsonProperty("icon")]
+        public string icon { get; set; }
+
+        [JsonProperty("code")]
+        public int code { get; set; }
+    }
+
+    public class Current
+    {
+
+        [JsonProperty("last_updated_epoch")]
+        public int last_updated_epoch { get; set; }
+
+        [JsonProperty("last_updated")]
+        public string last_updated { get; set; }
+
+        [JsonProperty("temp_c")]
+        public double temp_c { get; set; }
+
+        [JsonProperty("temp_f")]
+        public double temp_f { get; set; }
+
+        [JsonProperty("is_day")]
+        public int is_day { get; set; }
+
+        [JsonProperty("condition")]
+        public Condition condition { get; set; }
+
+        [JsonProperty("wind_mph")]
+        public double wind_mph { get; set; }
+
+        [JsonProperty("wind_kph")]
+        public double wind_kph { get; set; }
+
+        [JsonProperty("wind_degree")]
+        public int wind_degree { get; set; }
+
+        [JsonProperty("wind_dir")]
+        public string wind_dir { get; set; }
+
+        [JsonProperty("pressure_mb")]
+        public double pressure_mb { get; set; }
+
+        [JsonProperty("pressure_in")]
+        public double pressure_in { get; set; }
+
+        [JsonProperty("precip_mm")]
+        public double precip_mm { get; set; }
+
+        [JsonProperty("precip_in")]
+        public double precip_in { get; set; }
+
+        [JsonProperty("humidity")]
+        public int humidity { get; set; }
+
+        [JsonProperty("cloud")]
+        public int cloud { get; set; }
+
+        [JsonProperty("feelslike_c")]
+        public double feelslike_c { get; set; }
+
+        [JsonProperty("feelslike_f")]
+        public double feelslike_f { get; set; }
+
+        [JsonProperty("vis_km")]
+        public double vis_km { get; set; }
+
+        [JsonProperty("vis_miles")]
+        public double vis_miles { get; set; }
+
+        [JsonProperty("uv")]
+        public double uv { get; set; }
+    }
+
+    public class Day
+    {
+
+        [JsonProperty("maxtemp_c")]
+        public double maxtemp_c { get; set; }
+
+        [JsonProperty("maxtemp_f")]
+        public double maxtemp_f { get; set; }
+
+        [JsonProperty("mintemp_c")]
+        public double mintemp_c { get; set; }
+
+        [JsonProperty("mintemp_f")]
+        public double mintemp_f { get; set; }
+
+        [JsonProperty("avgtemp_c")]
+        public double avgtemp_c { get; set; }
+
+        [JsonProperty("avgtemp_f")]
+        public double avgtemp_f { get; set; }
+
+        [JsonProperty("maxwind_mph")]
+        public double maxwind_mph { get; set; }
+
+        [JsonProperty("maxwind_kph")]
+        public double maxwind_kph { get; set; }
+
+        [JsonProperty("totalprecip_mm")]
+        public double totalprecip_mm { get; set; }
+
+        [JsonProperty("totalprecip_in")]
+        public double totalprecip_in { get; set; }
+
+        [JsonProperty("avgvis_km")]
+        public double avgvis_km { get; set; }
+
+        [JsonProperty("avgvis_miles")]
+        public double avgvis_miles { get; set; }
+
+        [JsonProperty("avghumidity")]
+        public double avghumidity { get; set; }
+
+        [JsonProperty("condition")]
+        public Condition condition { get; set; }
+
+        [JsonProperty("uv")]
+        public double Uv { get; set; }
+    }
+
+    public class Astro
+    {
+
+        [JsonProperty("sunrise")]
+        public string sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public string sunset { get; set; }
+
+        [JsonProperty("moonrise")]
+        public string moonrise { get; set; }
+
+        [JsonProperty("moonset")]
+        public string moonset { get; set; }
+    }
+
+    public class Forecastday
+    {
+
+        [JsonProperty("date")]
+        public string date { get; set; }
+
+        [JsonProperty("date_epoch")]
+        public int date_epoch { get; set; }
+
+        [JsonProperty("day")]
+        public Day day { get; set; }
+
+        [JsonProperty("astro")]
+        public Astro astro { get; set; }
+    }
+
+    public class Forecast
+    {
+
+        [JsonProperty("forecastday")]
+        public IList<Forecastday> forecastday { get; set; }
+    }
+
+    public class Example
+    {
+
+        [JsonProperty("location")]
+        public Location location { get; set; }
+
+        [JsonProperty("current")]
+        public Current current { get; set; }
+
+        [JsonProperty("forecast")]
+        public Forecast forecast { get; set; }
+    }
+}
---
+++ Weather/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void city_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Example example = new Example();
+            using (var client = new WebClient())
+            {
+
+                string url = " http://api.apixu.com/v1/forecast.json?key=9c045e38b5e44c33934151952191102&q=" + city.Text + "&days=7";
+                example = JsonConvert.DeserializeObject<Example>(client.DownloadString(url));
+            }
+            //for (int i = 0; i < example.forecast.forecastday.Count(); i++)
+            //{
+            //    wind1.Content = "Скорость ветра:" + example.forecast.forecastday[i].
+            //}
+            //wind1.Content = "Скорость ветра: " + example.forecast.forecastday.date + " м/c";
+
+            //humidity1.Content = "Влажность: " + example.main.humidity;
+            //label1.Content = "Прогноз погоды в " + city.Text + " на 5 дней";
+            whichCity.Content = "Прогноз погоды в " + city.Text+" на неделю";
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri("http:" + example.forecast.forecastday[0].day.condition.icon);
+            image.EndInit();
+            Image1.Source = image;
+            wind1.Content = "Скорость ветра: " + example.forecast.forecastday[0].day.maxwind_kph+"км/ч";
+            humidity1.Content = "Влажность: " + example.forecast.forecastday[0].day.avghumidity+"%";
+            temperatureMorning1.Content = "Температура утром: " + example.forecast.forecastday[0].day.mintemp_c+ "°C";
+            temperatureNight1.Content = "Температура ночью: " + example.forecast.forecastday[0].day.maxtemp_c + "°C";
+            precip1.Content = "Осадки: " + example.forecast.forecastday[0].day.totalprecip_mm + "мм";
+            day1.Content = example.forecast.forecastday[0].date;
+
+            BitmapImage image2 = new BitmapImage();
+            image2.BeginInit();
+            image2.UriSource = new Uri("http:" + example.forecast.forecastday[1].day.condition.icon);
+            image2.EndInit();
+            Image2.Source = image2;
+            wind2.Content = "Скорость ветра: " + example.forecast.forecastday[1].day.maxwind_kph + "км/ч";
+            humidity2.Content = "Влажность: " + example.forecast.forecastday[1].day.avghumidity + "%";
+            temperatureMorning2.Content = "Температура утром: " + example.forecast.forecastday[1].day.mintemp_c + "°C";
+            temperatureNight2.Content = "Температура ночью: " + example.forecast.forecastday[1].day.maxtemp_c + "°C";
+            precip2.Content = "Осадки: " + example.forecast.forecastday[1].day.totalprecip_mm + "мм";
+            day2.Content = example.forecast.forecastday[1].date;
+
+            BitmapImage image3 = new BitmapImage();
+            image3.BeginInit();
+            image3.UriSource = new Uri("http:" + example.forecast.forecastday[2].day.condition.icon);
+            image3.EndInit();
+            Image3.Source = image3;
+            wind3.Content = "Скорость ветра: " + example.forecast.forecastday[2].day.maxwind_kph + "км/ч";
+            humidity3.Content = "Влажность: " + example.forecast.forecastday[2].day.avghumidity + "%";
+            temperatureMorning3.Content = "Температура утром: " + example.forecast.forecastday[2].day.mintemp_c + "°C";
+            temperatureNight3.Content = "Температура ночью: " + example.forecast.forecastday[2].day.maxtemp_c + "°C";
+            precip3.Content = "Осадки: " + example.forecast.forecastday[2].day.totalprecip_mm + "мм";
+            day3.Content = example.forecast.forecastday[2].date;
+
+            BitmapImage image4 = new BitmapImage();
+            image4.BeginInit();
+            image4.UriSource = new Uri("http:" + example.forecast.forecastday[3].day.condition.icon);
+            image4.EndInit();
+            Image4.Source = image4;
+            wind4.Content = "Скорость ветра: " + example.forecast.forecastday[3].day.maxwind_kph + "км/ч";
+            humidity4.Content = "Влажность: " + example.forecast.forecastday[3].day.avghumidity + "%";
+            temperatureMorning4.Content = "Температура утром: " + example.forecast.forecastday[3].day.mintemp_c + "°C";
+            temperatureNight4.Content = "Температура ночью: " + example.forecast.forecastday[3].day.maxtemp_c + "°C";
+            precip4.Content = "Осадки: " + example.forecast.forecastday[3].day.totalprecip_mm + "мм";
+            day4.Content = example.forecast.forecastday[3].date;
+
+            BitmapImage image5 = new BitmapImage();
+            image5.BeginInit();
+            image5.UriSource = new Uri("http:" + example.forecast.forecastday[4].day.condition.icon);
+            image5.EndInit();
+            Image5.Source = image5;
+            wind5.Content = "Скорость ветра: " + example.forecast.forecastday[4].day.maxwind_kph + "км/ч";
+            humidity5.Content = "Влажность: " + example.forecast.forecastday[4].day.avghumidity + "%";
+            temperatureMorning5.Content = "Температура утром: " + example.forecast.forecastday[4].day.mintemp_c + "°C";
+            temperatureNight5.Content = "Температура ночью: " + example.forecast.forecastday[4].day.maxtemp_c + "°C";
+            precip5.Content = "Осадки: " + example.forecast.forecastday[4].day.totalprecip_mm + "мм";
+            day5.Content = example.forecast.forecastday[4].date;
+
+            BitmapImage image6 = new BitmapImage();
+            image6.BeginInit();
+            image6.UriSource = new Uri("http:" + example.forecast.forecastday[5].day.condition.icon);
+            image6.EndInit();
+            Image6.Source = image6;
+            wind6.Content = "Скорость ветра: " + example.forecast.forecastday[5].day.maxwind_kph + "км/ч";
+            humidity6.Content = "Влажность: " + example.forecast.forecastday[5].day.avghumidity + "%";
+            temperatureMorning6.Content = "Температура утром: " + example.forecast.forecastday[5].day.mintemp_c + "°C";
+            temperatureNight6.Content = "Температура ночью: " + example.forecast.forecastday[5].day.maxtemp_c + "°C";
+            precip6.Content = "Осадки: " + example.forecast.forecastday[5].day.totalprecip_mm + "мм";
+            day6.Content = example.forecast.forecastday[5].date;
+        }
+
+        private void city_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void TextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            var cities = AllCity.GetCity();
+            string query = (sender as TextBox).Text;
+
+            hintCities.Children.Clear();
+
+            foreach (var obj in cities)
+            {
+                if (obj.ToLower().StartsWith(query.ToLower()))
+                {
+                    addItem(obj);
+                
+                }
+            }
+        }
+        private void addItem(string text)
+        {
+          
+
+            TextBlock block = new TextBlock();
+
+            block.Text = text;
+
+            block.Margin = new Thickness(2, 3, 2, 3);
+            block.Cursor = Cursors.Hand;
+
+            block.MouseLeftButtonUp += (sender, e) =>
+            {
+                city.Text = (sender as TextBlock).Text;
+                hintCities.Children.Clear();
+            };
+
+            block.MouseEnter += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.MediumPurple;
+            };
+
+            block.MouseLeave += (sender, e) =>
+            {
+                TextBlock b = sender as TextBlock;
+                b.Background = Brushes.Transparent;
+            };
+
+            hintCities.Children.Add(block);
+        }
+
+        private void CitySearch_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            city.Clear();
+        }
+    }
+}
